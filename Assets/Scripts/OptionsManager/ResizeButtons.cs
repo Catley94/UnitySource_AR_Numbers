a351576aa15cd064b3705ab36ac25e96e5c45afb
positive-season <scp94373@gmail.com>
2023-10-10 09:56:44 +0100

    Now resizes buttons according to the screen width

---
+++ Assets/Scripts/OptionsManager/ResizeButtons.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResizeButtons : MonoBehaviour
+{
+    #region Public
+    
+        
+    
+    #endregion
+    
+    #region Private
+
+        private void Start()
+        {
+            foreach (Transform _button in transform)
+            {
+                _button.localScale = new Vector3(Screen.width * 0.0025f, Screen.width * 0.0025f, 1f);
+            }
+        }
+
+    #endregion
+}
